cc69a5d6ebfbe17bfbc050ce5e5911ca0d6f17a8
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 17:02:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Artist
+  extend Memorable, Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@artists = []
+
+  def initialize
+    @@artists << self
+    @songs = []
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
+end
