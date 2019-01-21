69d6b8f2d1e4ad32f90892505781d17bc4a9bc2c
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 17:00:53 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,6 @@
 module Findable
 
     def find_by_name(name)
-      @@artists.detect{|a| a.name == name}
+      self.all.detect{|a| a.name == name}
     end
-
-    def find_by_name(name)
-      @@songs.detect{|a| a.name == name}
-    end
-
 end
