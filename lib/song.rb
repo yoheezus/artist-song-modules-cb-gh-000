673db4e64d1f2ff3800337ca0bc0cd42cc1852d2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 17:17:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,11 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods, Findable
   include Memorable::InstanceMethods, Paramable
-  attr_accessor :name
-  attr_reader :artist
+  attr_accessor :name, :artist
 
   @@songs = []
 
   def self.all
     @@songs
-  end
-
-  def artist=(artist)
-    @artist = artist
   end
 end
